53338be464f8d7bdb675aa94ab95a9c2c2070d5d
Lucas Fonseca Becari <l.becari@sidi.org.br>
2022-01-04 23:40:03 -0300

    Primeiro Commit

--- Missao4/Program.cs
+++ Missao4/Program.cs
@@ -10,6 +10,53 @@ namespace Missao4
     {
         static void Main(string[] args)
         {
+            // Variavel que armazena um número inteiro
+            int inteiro = 10;
+            Console.WriteLine($"valor da variável é: {inteiro}");
+            Console.WriteLine("O valor da variável é: " + inteiro);
+            Console.WriteLine();
+
+            // Variavel que armazena um número long
+            long longo = 16320;
+            Console.WriteLine($"valor da variável é: {longo}");
+            Console.WriteLine("O valor da variável é: " + longo);
+            Console.WriteLine();
+
+            // Variável que armazena double
+            double deci = 5.50;
+            Console.WriteLine($"valor da variável é: {deci}");
+            Console.WriteLine("O valor da variável é: " + deci);
+            Console.WriteLine();
+
+            //Variavel que armazena string
+            string nome = "Becari";
+            Console.WriteLine($"valor da variável é: {nome}");
+            Console.WriteLine("O valor da variável é: " + nome);
+            Console.WriteLine();
+
+            //Variavel que armazena verdadeiro ou falso
+            bool trueorfake = true;
+            Console.WriteLine($"valor da variável é: {trueorfake}");
+            Console.WriteLine("O valor da variável é: " + trueorfake);
+            Console.WriteLine();
+
+
+            List<string> carros = new List<string>();
+            carros.Add("Uno");
+            carros.Add("Gol");
+            carros.Add("Palio");
+
+            foreach (var carro in carros)
+            {
+                Console.WriteLine($"O valor da variável da lista de strings é: {carro}");
+            }
+
+
+            Console.ReadLine();
+
+
+
+
         }
     }
 }
